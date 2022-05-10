eea644e9f122880828b570a74868e6a77a8a7d02
Dhatrireddyy <102531062+Dhatrireddyy@users.noreply.github.com>
2022-05-10 10:20:43 +0530

    Update main.py

--- codes/main.py
+++ codes/main.py
@@ -1,3 +1,14 @@
+# CBSE Probability Grade 12
+# Exercise 13.2.5
+
+# Name: Velma Dhatri Reddy
+# Roll number: AI21BTECH11030
+
+""" Problem Statement
+A die marked 1,2,3 in red and 4,5,6 in green is tossed. Let A be the event,
+‘the number is even’, and B be the event, ‘the number is red’. Are A and B independent?
+"""
+
 import numpy as np
 import pandas as pd
 
